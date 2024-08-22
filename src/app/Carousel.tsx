13328b252b3648f6e13fc7fe870de0ae942e0713
jratul <jratul@nate.com>
2024-08-22 17:55:11 +0900

    add carousel in main page

---
+++ src/app/Carousel.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+
+function Zelda() {
+  return (
+    <p className="mt-4 text-xl text-white">
+      왕국의 눈물 출시 기념
+      <br />
+      젤다의 전설{" "}
+      <span className="font-semibold text-red-300">패키지 할인</span> 이벤트
+    </p>
+  );
+}
+
+function Pokemon() {
+  return (
+    <p className="mt-4 text-xl text-white">
+      포켓몬 매니아들을 위한
+      <br />
+      <span className="font-semibold text-red-300">레츠고 피카츄</span>가 포함된
+      <br />
+      알찬 구성이 준비되어 있습니다.
+    </p>
+  );
+}
+
+function AnimalCrossing() {
+  return (
+    <p className="mt-4 text-xl text-white">
+      <span className="font-semibold text-red-300">귀여운 동물들</span>을 만날
+      준비가 되셨나요?
+      <br />
+      스위치{" "}
+      <span className="font-semibold text-red-300">동물의 숲 에디션</span>까지!
+    </p>
+  );
+}
+
+const data = [
+  {
+    image: "/images/carousel/zelda-promo.jpg",
+    title: "젤다의 전설",
+    href: "#",
+    content: <Zelda />,
+  },
+  {
+    image: "/images/carousel/pokemon.jpg",
+    title: "포켓몬",
+    href: "#",
+    content: <Pokemon />,
+  },
+  {
+    image: "/images/carousel/animal-crossing.jpg",
+    title: "동물의 숲",
+    href: "#",
+    content: <AnimalCrossing />,
+  },
+];
+
+export default function Carousel() {
+  const [idx, setIdx] = useState<number>(0);
+
+  const handleLeft = () => {
+    setIdx(idx - 1 < 0 ? data.length - 1 : idx - 1);
+  };
+
+  const handleRight = () => {
+    setIdx(idx + 1 > data.length - 1 ? 0 : idx + 1);
+  };
+
+  return (
+    <div className="h-[70vh] overflow-hidden">
+      <div
+        className={`w-[${
+          100 * data.length
+        }vw] transition-transform ease-out duration-500 -translate-x-[${
+          100 * idx
+        }vw]`}
+      >
+        {data.map((item, idx) => (
+          <div className="w-screen float-left relative" key={idx}>
+            <div className="absolute inset-0 bg-gray-900 opacity-70" />
+            <div className="absolute w-full h-full">
+              <div className="relative mx-auto flex flex-col items-center py-[20vh] text-center">
+                <h1 className="text-4xl font-bold text-red-600 lg:text-6xl">
+                  {item.title}
+                </h1>
+                {item.content}
+                <a
+                  href={item.href}
+                  className="mt-8 rounded-md bg-red-500 px-8 py-3 text-base font-medium text-gray-200 hover:bg-red-400"
+                >
+                  보러가기
+                </a>
+              </div>
+            </div>
+            <img
+              src={item.image}
+              alt={item.title}
+              className="object-cover w-screen"
+            />
+          </div>
+        ))}
+      </div>
+      <ChevronLeftIcon
+        className="w-20 h-20 absolute text-red-500 text-5xl font-bold left-2 top-[30vh] cursor-pointer z-10 hover:text-red-300"
+        onClick={handleLeft}
+      />
+      <ChevronRightIcon
+        className="w-20 h-20 absolute text-red-500 text-5xl font-bold right-2 top-[30vh] cursor-pointer z-10 hover:text-red-300"
+        onClick={handleRight}
+      />
+    </div>
+  );
+}
