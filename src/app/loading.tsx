5ed8a1bcb4c0a470547c82f5fbac0ddaa9324c1c
jratul <jratul@nate.com>
2026-07-03 06:24:21 +0900

    fix: 페이지 전환 시 로딩 스피너가 nav바에 붙어 보이던 문제 수정
    min-h-[50vh] 컨테이너에 flex가 없어 m-auto가 아무 효과도 내지 못하고
    있었음(플렉스 부모 없이는 m-auto가 중앙 정렬을 하지 않음). flex
    items-center justify-center로 실제 중앙 정렬되도록 수정.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,10 +2,8 @@ import Spinner from "@/components/Spinner";
 
 export default function Loading() {
   return (
-    <div className="min-h-[50vh]">
-      <div className="m-auto">
-        <Spinner size={32} />
-      </div>
+    <div className="min-h-[50vh] flex items-center justify-center">
+      <Spinner size={32} />
     </div>
   );
 }
